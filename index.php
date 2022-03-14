dac6f3f9c96eb46e030bbb09eb45992438e80d67
jeanfmarques <jean.tapera@gmail.com>
2022-03-14 18:44:05 -0300

    Versão 14/03

--- index.php
+++ index.php
@@ -4,6 +4,17 @@ require __DIR__.'/vendor/autoload.php';
 
 use \App\Entity\Vaga;
 
+//BUSCA
+$busca = filter_input(INPUT_GET, 'busca', FILTER_SANITIZE_STRING);
+
+//CONDIÇÕES SQL
+$condicoes = [
+	strlen($busca) ? 'titulo LIKE "%'.$busca.'%"' : null	
+];
+
+//CLAUSULA WHERE
+$where = implode(' AND ', $condicoes);
+
 $vagas = Vaga::getVagas();
 
 include __DIR__.'/includes/header.php';
