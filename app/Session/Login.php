f1f6ee337f6f907f2eb588a77b7ec2df723aae77
jeanfmarques <jean.tapera@gmail.com>
2022-03-29 20:23:21 -0300

    Alterações iniciais do Login

---
+++ app/Session/Login.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Session;
+
+class Login{
+    /**
+     * Método responsável por verificar se o usuário está logado
+     * @return boolean
+     */
+    public static function isLogged(){
+        return false;
+    } 
+    /**
+     * Método responsável por obrigar o usuário a estar logado para acessar
+     */
+    public static function requireLogin(){
+        if(!self::isLogged()){
+            header('location: login.php');
+            exit;
+        }
+
+    }
+    /**
+     * Método responsável por obrigar o usuário a estar logado para acessar
+     */
+    public static function requireLogout(){
+        if(self::isLogged()){
+            header('location: index.php');
+            exit;
+        }
+
+    }
+
+}
+
--- excluir.php
+++ excluir.php
@@ -3,6 +3,10 @@
 require __DIR__.'/vendor/autoload.php';
 
 use \App\Entity\Vaga;
+use \App\Session\Login;
+
+//OBRIGA O USUÁRIO A ESTAR LOGADO
+Login::requireLogin();
 
 //VALIDAÇÃO DO ID
 if(!isset($_GET['id']) or !is_numeric($_GET['id'])){
